d58d3a11c7ec88071cc3d253e67bfc841aab9ef9
Camila <camilafonseca.ang7@gmail.com>
2025-10-16 19:32:05 -0300

    feat: ajustes de layout e navegacao

--- src/components/templates/Header/Header.tsx
+++ src/components/templates/Header/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useMemo } from "react";
 
 import Code from "@/components/icons/Code";
@@ -8,6 +8,8 @@ import { appRoutes } from "@/constants/routes";
 import { usePosts } from "@/services/hooks/usePosts";
 
 const Header = () => {
+  const router = useRouter();
+
   const pathname = usePathname();
 
   const { data } = usePosts();
@@ -16,13 +18,21 @@ const Header = () => {
 
   return (
     <header className="flex justify-between py-8 text-2xl heading gap-4">
-      <div className="flex items-center gap-2 md:gap-5">
-        <Code className="w-5 md:w-11" />
+      <button
+        type="button"
+        className="flex items-center cursor-pointer"
+        onClick={() => {
+          router.push(appRoutes.home);
+        }}
+      >
+        <div className="flex items-center gap-2 md:gap-5">
+          <Code className="w-5 md:w-11" />
 
-        <h1 className="uppercase text-[1rem] md:text-[1.5rem]">
-          Fernanda Mascheti
-        </h1>
-      </div>
+          <h1 className="uppercase text-[1rem] md:text-[1.5rem]">
+            Fernanda Mascheti
+          </h1>
+        </div>
+      </button>
 
       <nav aria-label="Menu" className="flex items-center">
         <ul className="flex items-center gap-3 md:gap-8">
