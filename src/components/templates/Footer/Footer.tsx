00a652b3714f74713ecc3d3763c9ac6fb697ca42
Camila <camilafonseca.ang7@gmail.com>
2025-10-16 18:40:31 -0300

    refactor: usar prop correta e tamanho do texto no footer

--- src/components/templates/Footer/Footer.tsx
+++ src/components/templates/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => (
   <footer>
-    <p className="w-full text-center paragraph mt-16">
+    <p className="w-full text-center paragraph mt-16 text-[0.8rem] lg:text-[1rem]">
       &copy; Copyright 2025. Produzido por Fernanda Mascheti
     </p>
   </footer>
