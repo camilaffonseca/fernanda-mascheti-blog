b1abd51a45c0628404bd323a5d262a7e84502988
Camila <camilafonseca.ang7@gmail.com>
2025-10-16 09:43:56 -0300

    fix: correçao de importaçao

--- src/services/http/posts.ts
+++ src/services/http/posts.ts
@@ -1,8 +1,11 @@
-import type { Metadata, Pagination } from "@/models/commonDataTraffic";
+import type {
+  Metadata,
+  Pagination,
+  PaginationQueryParams,
+} from "@/models/commonDataTraffic";
 import type { PostModel } from "@/models/post";
 
 import { api } from "@/services/fetchers/api";
-import type { PaginationQueryParams } from "@/services/types/pagination";
 
 type PostsResponse = {
   posts?: PostModel[];
